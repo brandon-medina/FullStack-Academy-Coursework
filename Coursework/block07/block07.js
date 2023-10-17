f365b060684f88be0382f2330ee632ba28b3a632
brandon-medina <147010981+brandon-medina@users.noreply.github.com>
2023-10-16 23:32:12 -0500

    Add files via upload

---
+++ Coursework/block07/block07.js
@@ -0,0 +1,14 @@
+//Pseudocode: Create a string for the user saying: You have received this message because you have been chosen to open an important vault. Here is the secret combination: 
+const greeting = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination:'
+
+//Pseudocode: Create a variable that stores the result of a calculation using one or more of the four basic arithmetic operators (+, -, *, /) and equals 10.
+let code1 = 5 * 2;
+
+//Pseudocode: Create a variable that stores the result of a calculation using one or more of the four basic arithmetic operators (+, -, *, /) and equals 40.
+let code2 = (50 - 15) / 7 * 8;
+
+//Pseudocode: Create a variable that stores the result of a calculation using one or more of the four basic arithmetic operators (+, -, *, /) and equals 39.
+let code3 = (100 - 50) / 2 + 14;
+
+//Pseudocode: Create a popup box that strings together the greeting and the three secret codes. Be sure to purposefully include "spaces" in the string between each variable.
+alert(greeting + " " + code1 + " " + code2 + " " + code3);
---
+++ Coursework/block07/guidedpractice.js
@@ -0,0 +1,18 @@
+//Pseudocode: Create a dialog box that says Grab a toothbrush with your hand. 
+alert("Grab a toothbrush with your hand.")
+//Pseudocode: Create a dialog box that says Turn on the water. 
+alert("Turn on the water.")
+//Pseudocode: Create a dialog box that says Rinse the brush part of the toothbrush with water.
+ alert("Rinse the brush part of the toothbrush with water.")
+//PSeudocode: Turn off the water. 
+//Pseudocode: Open the tube of toothpaste. 
+//Pseudocode: Squeeze toothpaste on the brush part of the toothbrush. 
+//Pseudocode: Start with the front teeth. 
+//Pseudocode: Move in circular motions with the brush.
+let numCows = 50;
+const x = "Hello World";
+console.log("Hello World");
+console.log("Hello, " + "Disney Land!");
+const numUmbrella = 10*3;
+var num = 5+1;
+console.log(num);
